190de3d5b6a7330fe95e430146255b31c8fac42d
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-10-10 13:51:36 +0300

    last fix

--- Scripts/script1.sql
+++ Scripts/script1.sql
@@ -1,1 +1,8 @@
-select 1;  
+DROP TABLE `customers`;
+
+CREATE TABLE `customers` (
+  `customer_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `surname` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  PRIMARY KEY (`customer_id`));
